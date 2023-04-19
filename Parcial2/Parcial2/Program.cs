02b657060a5c83d942174e497d75175f8e081b8b
Rosavalle <125718156+Rosavalle@users.noreply.github.com>
2023-04-18 21:23:15 -0600

    ejercicio de parcial

---
+++ Parcial2/Parcial2/Program.cs
@@ -0,0 +1,12 @@
+﻿int datol = 16;
+string dato2 = "25g";
+
+try
+{
+    int resultado = datol + Convert.ToInt32(dato2);
+    Console.WriteLine(resultado);
+}
+catch (Exception)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
